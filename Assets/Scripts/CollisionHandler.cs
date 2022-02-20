b84747cf665941e0b8601f69f20fa1580fc3a007
MagneticRegulus <ladymeckbert@gmail.com>
2022-02-20 22:55:09 +1300

    Basic movement, crashing, success, and scene progression

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,106 @@
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    [SerializeField] float sceneChangeDelay = 2f;
+    [SerializeField] AudioClip crashAudio;
+    [SerializeField] AudioClip successAudio;
+    [SerializeField] ParticleSystem crashParticles;
+    [SerializeField] ParticleSystem successParticles;
+
+    AudioSource audioSource;
+
+    bool isTransitioning = false;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // if we haven't already crashed or succeeded
+        if (isTransitioning)
+        {
+            return;
+        }
+
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                break;
+            case "Finish":
+                StartSuccessSequence();
+                break;
+            default:
+                StartCrashSequence();
+                break;
+        }
+    }
+    
+    void StartCrashSequence()
+    {
+        isTransitioning = true;
+
+        PlayAudio(crashAudio);
+        crashParticles.Play();
+
+        SceneChange(nameof(ReloadScene)); // use nameof instead of string
+    }
+
+    void ReloadScene()
+    {
+        var activeScene = SceneManager.GetActiveScene();
+        // SceneManager.LoadScene(activeScene.name);
+        SceneManager.LoadScene(activeScene.buildIndex);
+    }
+
+    void StartSuccessSequence()
+    {
+        isTransitioning = true;
+
+        PlayAudio(successAudio);
+        successParticles.Play();
+
+        SceneChange(nameof(LoadNextScene));
+    }
+
+    void LoadNextScene()
+    {
+        var activeScene = SceneManager.GetActiveScene();
+        var sceneCount = SceneManager.sceneCountInBuildSettings;
+        var nextSceneIndex = activeScene.buildIndex + 1;
+
+        // This reads better to me than the example given but appreciate it looks like reusing code.
+        // explicitly indicates whether or not we're loading the next level or the level at index 1.
+        if (nextSceneIndex < sceneCount)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    void SceneChange(string methodName)
+    {
+        var movement = gameObject.GetComponent<Movement>();
+        movement.StopAllParticles();
+        movement.enabled = false;
+
+        Invoke(methodName, sceneChangeDelay);
+    }
+
+    void PlayAudio(AudioClip audioClip)
+    {
+        if (audioSource.isPlaying)
+        {
+            audioSource.Stop();
+        }
+
+        audioSource.PlayOneShot(audioClip);
+    }
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,18 +1,30 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    Rigidbody rb;
     [SerializeField] float mainThrust = 1000f;
     [SerializeField] float rotationThrust = 1000f;
+    [SerializeField] AudioClip thrustAudio;
+
+    [SerializeField] ParticleSystem mainThrustParticles;
+    [SerializeField] ParticleSystem rightThrustParticles;
+    [SerializeField] ParticleSystem leftThrustParticles;
+
+    Rigidbody rb;
+    AudioSource audioSource;
+
+    public void StopAllParticles()
+    {
+        mainThrustParticles.Stop();
+        rightThrustParticles.Stop();
+        leftThrustParticles.Stop();
+    }
     
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -27,7 +39,31 @@ public class Movement : MonoBehaviour
         // GetKey -> hold down, GetKeyDown -> one frame
         if (Input.GetKey(KeyCode.Space))
         {
-            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+            Thrust();
+        }
+        else
+        {
+            StopThrusting();
+        }
+    }
+
+    private void StopThrusting()
+    {
+        audioSource.Stop();
+        mainThrustParticles.Stop();
+    }
+
+    private void Thrust()
+    {
+        rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(thrustAudio);
+        }
+
+        if (!mainThrustParticles.isPlaying)
+        {
+            mainThrustParticles.Play();
         }
     }
 
@@ -36,17 +72,31 @@ public class Movement : MonoBehaviour
         // Default left
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
-            ApplyRotation(Vector3.forward);
+            ApplyRotation(Vector3.forward, rightThrustParticles); // use opposite particles to rotation direction
         }
         else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
-            ApplyRotation(Vector3.back);
+            ApplyRotation(Vector3.back, leftThrustParticles); // use opposite particles to rotation direction
+        }
+        else
+        {
+            StopRotating();
         }
     }
 
-    void ApplyRotation(Vector3 vector3)
+    private void StopRotating()
+    {
+        rightThrustParticles.Stop();
+        leftThrustParticles.Stop();
+    }
+
+    void ApplyRotation(Vector3 vector3, ParticleSystem particleSystem)
     {
         rb.freezeRotation = true; // freezing rotation so we can manually rotate
+        if (!particleSystem.isPlaying)
+        {
+            particleSystem.Play();
+        }
         transform.Rotate(vector3 * rotationThrust * Time.deltaTime);
         rb.freezeRotation = false; // unfreezing rotation so physics system can take over
     }
